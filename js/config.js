1fa1fcb36f7be888b97bc10e3e5d22f4940eec6f
robinsonneil50 <70523940+robinsonneil50@users.noreply.github.com>
2021-12-29 17:46:13 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'eu-west-1_FIH2JYlDG', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '6lgtb0vr86md81pq6dj2mhf79i', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
